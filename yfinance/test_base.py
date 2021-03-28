48514e7b96814ed1d3dad8799983d18aceecf2ce
walkP <wpeters@ualberta.ca>
2021-03-28 10:58:21 -0600

    added Ji Heon's tests that he wrote, I just cleaned stuff up

--- yfinance/test_base.py
+++ yfinance/test_base.py
@@ -1,8 +1,78 @@
 import unittest
+import base
+import utils
 
-class TestGenericPatterns(unittest.TestCae):
-    def test_generic_pattern(self):
-        pass
-    
-if __name__ == '_get_fundamentals':
-    unittest._get_fundamentals()
+class TestGenericPatterns(unittest.TestCase):
+    # testing if data is a dict type
+    def test_is_dict(self):
+        msft = base.TickerBase(ticker="MSFT")
+        empty_dict = dict()
+        data = msft.generic_patterns(empty_dict)
+        self.assertTrue(isinstance(data, dict))
+
+    # testing if loop is being handled properly
+    def test_loop(self):
+        data = {}
+        alist = []
+        _financials = {
+            "yearly": utils.empty_df(),
+            "quarterly": utils.empty_df()}
+        _balancesheet = {
+            "yearly": utils.empty_df(),
+            "quarterly": utils.empty_df()}
+        _cashflow = {
+            "yearly": utils.empty_df(),
+            "quarterly": utils.empty_df()}
+        for key in (
+            (_cashflow, 'cashflowStatement', 'cashflowStatements'),
+            (_balancesheet, 'balanceSheet', 'balanceSheetStatements'),
+            (_financials, 'incomeStatement', 'incomeStatementHistory')
+        ):
+            item = key[1] + 'History'
+            alist.append(item)
+        for i in range(len(alist)):
+            if i == 0:
+                self.assertTrue(alist[i] == "cashflowStatementHistory")
+            elif i == 1:
+                self.assertTrue(alist[i] == "balanceSheetHistory")
+            elif i == 2:
+                self.assertTrue(alist[i] == "incomeStatementHistory")
+
+    # testing if item doesn't exist in the data dictionary
+    def test_switch_statement(self):
+        data = {}
+        msg = ""
+        _financials = {
+            "yearly": utils.empty_df(),
+            "quarterly": utils.empty_df()}
+        _balancesheet = {
+            "yearly": utils.empty_df(),
+            "quarterly": utils.empty_df()}
+        _cashflow = {
+            "yearly": utils.empty_df(),
+            "quarterly": utils.empty_df()}
+        for key in (
+            (_cashflow, 'cashflowStatement', 'cashflowStatements'),
+            (_balancesheet, 'balanceSheet', 'balanceSheetStatements'),
+            (_financials, 'incomeStatement', 'incomeStatementHistory')
+        ):
+            item = key[1] + 'History'
+            if isinstance(data.get(item), dict):
+                try:
+                    msg += base.cleanup('yearly')
+                except Exception as e:
+                    pass
+
+            item = key[1]+'HistoryQuarterly'
+            if isinstance(data.get(item), dict):
+                try:
+                    msg += base.cleanup('quarterly')
+                except Exception as e:
+                    pass
+
+        if (not isinstance(data.get(item), dict)):
+            msg += "Empty DataFrame"
+        self.assertTrue(msg == "Empty DataFrame")
+
+if __name__ == "__main__":
+    unittest.main() 
